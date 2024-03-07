f729667d4ad71299c1c66f2aa77befe5d52b1ccd
arturdenderski <artur.denderski@gmail.com>
2024-03-07 10:26:20 +0000

    task4.1

---
+++ lab4/web-driver/src/test/java/com/example/webdriver/WebDriverApplicationTests.java
@@ -0,0 +1,45 @@
+package com.example.webdriver;
+
+import io.github.bonigarcia.seljup.SeleniumJupiter;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SeleniumJupiter.class)
+class WebDriverApplicationTests {
+
+
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setupClass() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setup() {
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    void test(ChromeDriver driver) {
+        // Exercise
+        String sutUrl = "https://www.youtube.com/";
+        driver.get(sutUrl);
+        String title = driver.getTitle();
+
+        // Verify
+        assertThat(title).isEqualTo("YouTube");    }
+
+    @AfterEach
+    void teardown() {
+        driver.quit();
+    }
+
+}
